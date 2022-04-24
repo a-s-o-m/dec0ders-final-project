62197525fbcf7532fefe9b0babdf25614096a75c
Renee Catanach <rcat@techexchange.in>
2022-04-24 20:55:31 +0000

    Attempt to add users email to database

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, request, redirect, url_for
 from model import User, get_recipes, get_recipes_test
-from sys import stderr
-
 
 # App variables
 app = Flask(__name__)
@@ -11,20 +9,20 @@ app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
 # Accessed from CONFIG VARS
-app.config['MONGO_URI'] = "mongodb+srv://test:test@finalproject.wnetq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = "mongodb+srv://test:<-password->@finalproject.wnetq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 #Initialize PyMongo
 mongo = PyMongo(app)
 
 user = User('demo@user.com')
 recipes = dict()
-users = mongo.db.users
 
 # HOME Route
 @app.route('/')
 @app.route('/home')
 def home(): 
     return render_template('home.html')
+
 
 @app.route('/search')
 def search():
@@ -35,7 +33,6 @@ def shopping_list():
     global user
     if request.method == 'POST':
         user.add_to_grocery_list(request.form['missing_ing'])
-        users.update_one({'email': user.email}, {'$set':{'grocery_list': user.get_grocery_list()}})
     return render_template('grocery-list.html', grocery_list = user.get_grocery_list(), user_email = user.email, email_body = user.get_grocery_list_email_body())
 
 @app.route('/my-recipes', methods=['GET','POST'])
@@ -57,20 +54,19 @@ def user_recipe():
 
     return render_template('recipe.html', recipe=recipe)
 
-@app.route('/email', methods=['POST'])
+@app.route('/home', methods=['GET','POST'])
 def email(): 
     # # find user with email
     users = mongo.db.users
-    #         #search for username/email in database\
+    #         #search for username/email in database
     existing_user = users.find_one({'email': request.form['email']})
     global user
     if not existing_user:
         user = User(request.form['email'])
         users.insert_one(user.to_doc())
     # # transform to user object using from_doc method
-    else: 
-        user = User.from_doc(existing_user)   
-    return redirect('/search_page.html')
+    else: user = User.from_doc(existing_user)   
+    return render_template('search_page.html')
     
 if __name__=='__main__':
     app.run(debug=True)
